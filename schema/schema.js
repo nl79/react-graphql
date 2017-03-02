4e24ceb4e35eaaad7d32a290b8d8091a1bd1b07b
Nazar Lesiv <nazarlesiv@Nazars-MBP.home>
2017-03-01 21:33:48 -0500

    Add and Delete data example.

--- schema/schema.js
+++ schema/schema.js
@@ -5,7 +5,8 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLList,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLNonNull
 } = graphql;
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
@@ -97,16 +98,27 @@ const mutation = new GraphQLObjectType({
     addUser: {
       type: UserType,
       args: {
-        firstName: { type: GraphQLString },
-        age: {type: GraphQLInt },
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        age: {type: new GraphQLNonNull(GraphQLInt) },
         companyId: {type: GraphQLString }
       },
-      resolve() {
-        
+      resolve(parentValue, {firstName, age}) {
+        return axios.post('http://localhost:3000/users', {firstName, age})
+        .then(res => res.data);
+      }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString)}
+      },
+      resolve(parentValue, {id}) {
+        return axios.delete(`http://localhost:3000/users/${id}`).then(res => res.data);
       }
     }
   }
 });
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation
 });
