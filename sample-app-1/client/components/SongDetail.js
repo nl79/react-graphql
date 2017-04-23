3b8005c89d8df346d190a1769164bcc1612302f6
Nazar Lesiv <nazarlesiv@Nazars-MBP.home>
2017-04-23 19:03:17 -0400

    Update Lyrics and add a Like Counter

--- sample-app-1/client/components/SongDetail.js
+++ sample-app-1/client/components/SongDetail.js
@@ -3,6 +3,9 @@ import { graphql } from 'react-apollo';
 import fetchSong from '../queries/fetchSong';
 import {Link} from 'react-router';
 
+import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
+
 class SongDetail extends Component {
   render() {
     let { song } = this.props.data;
@@ -12,6 +15,8 @@ class SongDetail extends Component {
       <div>
         <Link to='/'>Back</Link>
         <h3>{song.title}</h3>
+        <LyricList lyrics={song.lyrics}/>
+        <LyricCreate songId={this.props.params.id} />
       </div>
     );
   }
